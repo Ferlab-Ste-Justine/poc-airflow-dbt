03665d9f10a0ed92d5d285a52869a9dc9dc2c4a5
kritchie <ritchie.karl@gmail.com>
2025-02-21 13:14:09 -0500

    feat: [SJRA-62] (WIP) Add occurrences dbt pipeline

---
+++ radiant/sjra-62/dbt/models/staging/stg_normalized_snv.sql
@@ -0,0 +1,89 @@
+{{
+    config(
+        tags=['stg_normalized_snv'],
+        materialized='table',
+        table_type='PRIMARY',
+        keys=['part', 'seq_id', 'locus_id'],
+        engine='OLAP',
+        partition_type='Expr',
+        partition_by=['(`part`)'],
+        distributed_by=['locus_id'],
+        buckets='5 ',
+        properties="{'compression': 'LZ4', 'replication_num': '3', 'colocate_with': 'group_locus_id5'}",
+    )
+}}
+
+with occurrences as (
+    select
+      *
+    from {{ source('cqdg_datalake', 'normalized_snv') }}
+    where
+        study_id = 'cag'  -- TODO: SHOULD WE KEEP THIS ??
+    and
+        batch = 'annotated_vcf_cqdg_3'  -- TODO: SHOULD WE KEEP THIS ??
+    limit 100   -- TODO : REMOVE ME!
+),
+
+seq as (
+    select
+       ldm_sample_id
+       , seq_id
+       , part
+    from
+        {{ source('starrocks', 'sequencing_experiment') }} s
+    where
+        part
+    in
+        (58, 59, 60)  -- TODO: SHOULD WE KEEP THIS ??
+),
+
+final as (
+    select
+        s.part as `part`
+        , s.seq_id as seq_id
+        , dict_mapping('variant_dict', sha2(concat_ws('-', chromosome, start, reference, alternate), 256)) as locus_id
+        , o.ad_ratio
+        , o.ad_total
+        , o.ad_ref
+        , o.ad_alt
+        , o.dp
+        , o.gq
+        , o.chromosome
+        , o.`start`
+        , o.zygosity
+        , o.has_alt
+        , o.quality
+        , o.variant_class
+        , o.`filter`
+        , o.info_ac
+        , o.info_an
+        , o.info_af
+        , o.info_baseq_rank_sum
+        , o.info_excess_het
+        , o.info_fs
+        , o.info_ds
+        , o.info_fraction_informative_reads
+        , o.info_inbreed_coeff
+        , o.info_mleac
+        , o.info_mleaf
+        , o.info_mq
+        , o.info_m_qrank_sum
+        , o.info_qd
+        , o.info_r2_5p_bias
+        , o.info_read_pos_rank_sum
+        , o.info_sor
+        , o.info_vqslod
+        , o.info_culprit
+        , o.info_dp
+        , o.info_haplotype_score
+        , o.calls
+    from
+        occurrences o
+    join
+        [BROADCAST]
+        seq s
+    on
+        s.ldm_sample_id = o.sample_id
+)
+
+select * from final
